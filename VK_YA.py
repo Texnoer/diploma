53e11d0153a2897092e47d5304be43c47356518d
PauL <texnoer@ro.ru>
2021-01-25 21:12:18 +0300

    likes

--- VK_YA.py
+++ VK_YA.py
@@ -15,19 +15,26 @@ class VkUser:
             'access_token': self.token,
             'v': self.version
         }
-        # self.owner_id = you_id
+
     def get_fotos(self):
-        # if user_id is None:
-        #     user_id = self.owner_id
+        # Параметры VK
         foto_url = self.url + 'photos.get'
         foto_params = {
             'owner_id': you_id,
             'album_id': 'profile',
-            'count': '3',
+            'count': '5',
             'photo_sizes': True,
+            'extended': True
             }
         photos = requests.get(foto_url, params={**self.params, **foto_params})
         return photos.json()['response']['items']
+    def get_likes(self):
+        #Показывает кол-во лайков в фото
+        list_likes =[]
+        for photo in self.get_fotos():
+            likes = photo['likes']['count']
+            list_likes.append(likes)
+        return list_likes
 
     def get_largest(self, size_dict):
         # Функция ищет наибольший размер картинки
@@ -38,18 +45,20 @@ class VkUser:
 
     def sizes_max(self):
         # Цикл поиска фото наибольшего размера
+        list_photo = []
         for photo in self.get_fotos():
             sizes = photo['sizes']
             max_size = max(sizes, key=self.get_largest)['url']
-            return max_size
+            list_photo.append(max_size)
+        return list_photo
 
 vk_foto = VkUser(token, '5.126')
 
 # pprint(vk_foto.get_fotos())
 # pprint(vk_foto.get_fotos())#['response']['items'])
 pprint(vk_foto.sizes_max())
-# ['response']['items'][0]['sizes'][-1]
-
+pprint(vk_foto.get_likes()) #Лайки
 if __name__ == '__main__':
         VkUser(token, '5.126')
+
 
