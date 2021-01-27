268facfd5913abfe31120f93d25dc8ba55b03ddc
PauL <texnoer@ro.ru>
2021-01-27 20:15:13 +0300

    Ya

--- VK_YA.py
+++ VK_YA.py
@@ -5,7 +5,7 @@ with open('token.txt', 'r') as file_object:
     token = file_object.read().strip()
 
 you_id = int(input('Введите id: '))
-# token_ya = input('Вставте токен с Полигона Яндекс.Диска: ')
+token_ya = input('Вставте токен с Полигона Яндекс.Диска: ')
 class VkUser:
     url = 'https://api.vk.com/method/'
     def __init__(self, token, version):
@@ -28,13 +28,6 @@ class VkUser:
             }
         photos = requests.get(foto_url, params={**self.params, **foto_params})
         return photos.json()['response']['items']
-    def get_likes(self):
-        #Показывает кол-во лайков в фото
-        list_likes =[]
-        for photo in self.get_fotos():
-            likes = photo['likes']['count']
-            list_likes.append(likes)
-        return list_likes
 
     def get_largest(self, size_dict):
         # Функция ищет наибольший размер картинки
@@ -49,16 +42,47 @@ class VkUser:
         for photo in self.get_fotos():
             sizes = photo['sizes']
             max_size = max(sizes, key=self.get_largest)['url']
-            list_photo.append(max_size)
+            list_photo.append({'url': max_size, 'likes': photo['likes']['count']})
+
         return list_photo
+
 
 vk_foto = VkUser(token, '5.126')
 
 # pprint(vk_foto.get_fotos())
-# pprint(vk_foto.get_fotos())#['response']['items'])
-pprint(vk_foto.sizes_max())
-pprint(vk_foto.get_likes()) #Лайки
-if __name__ == '__main__':
-        VkUser(token, '5.126')
 
+pprint(vk_foto.sizes_max())
+
+
+
+# if __name__ == '__main__':
+#         VkUser(token, '5.126')
+HEADERS = {
+    "Authorization": f"OAuth {token_ya}"
+}
+class YaUploader:
+    def __init__(self, token_ya):
+        self.token_ya = token_ya
+
+    def upload(self, file_path: str):
+        """Метод загруджает файл file_path на яндекс диск"""
+        response = requests.get(
+            "https://cloud-api.yandex.net/v1/disk/resources/upload",
+
+            params={
+                "path": '/VK_Photo/21',
+
+            },
+            headers=HEADERS
+        )
+        href = response.json()["href"]
+
+        with open("            ", "rb") as f:
+            upload_response = requests.put(href, files={"file": f})
+        response.raise_for_status()
+        return 'Вернуть ответ об успешной загрузке'
+
+if __name__ == '__main__':
+    uploader = YaUploader("token_ya")
+    result = uploader.upload('url')
 
