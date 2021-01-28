71b21dce638d859678100e167850ecd0c1f32b6b
PauL <texnoer@ro.ru>
2021-01-28 22:53:29 +0300

    Ya_likes and photos many

--- VK_YA.py
+++ VK_YA.py
@@ -17,7 +17,7 @@ class VkUser:
         }
 
     def get_fotos(self):
-        # Параметры VK
+        """Параметры VK"""
         foto_url = self.url + 'photos.get'
         foto_params = {
             'owner_id': you_id,
@@ -30,14 +30,14 @@ class VkUser:
         return photos.json()['response']['items']
 
     def get_largest(self, size_dict):
-        # Функция ищет наибольший размер картинки
+        """Функция ищет наибольший размер картинки"""
         if size_dict['width'] >= size_dict['height']:
             return size_dict['width']
         else:
             return size_dict['height']
 
     def sizes_max(self):
-        # Цикл поиска фото наибольшего размера
+        """Цикл поиска фото наибольшего размера"""
         self.list_photo = []
         for photo in self.get_fotos():
             sizes = photo['sizes']
@@ -47,15 +47,6 @@ class VkUser:
 
 vk_foto = VkUser(token, '5.126')
 foto_likes = vk_foto.sizes_max()
-for pair in foto_likes:
-    url = pair['url']
-    likes = pair['likes']
-    print(likes)
-    print(url)
-# pprint(vk_foto.get_fotos())
-
-# likes_name = vk_foto.sizes_max()[0]['likes']
-# url_name = vk_foto.sizes_max()[0]['url']
 
 HEADERS = {
     "Authorization": f"OAuth {token_ya}"
@@ -66,7 +57,10 @@ class YaUploader:
 
     def upload(self, file_path: str):
         """Метод загруджает файл file_path на яндекс диск"""
-        response = requests.post(
+        for pair in foto_likes:
+            url = pair['url']
+            likes = pair['likes']
+            response = requests.post(
             "https://cloud-api.yandex.net/v1/disk/resources/upload",
             params={
                 "path": f'/VK_Photo/{likes}',
